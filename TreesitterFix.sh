785b08fcbf5802bcfbe9f268ac10dede6303f3da
ahumayde <ahumayde@gmail.com>
2023-09-22 01:13:20 +0100

    fixed bash script

--- TreesitterFix.sh
+++ TreesitterFix.sh
@@ -1,10 +1,6 @@
-1. Change directory and cd into telescope.nvim 
-2. Checkout to the master branch
+# 1. Change directory and cd into telescope.nvim 
+# 2. Checkout to the master branch
 
-full command:
+# full command:
 cd ~/.local/share/nvim/site/pack/packer/start/telescope.nvim/ && git checkout master && git pull
-
-seperated command:
-cd ~/.local/share/nvim/site/pack/packer/start/telescope.nvim/
-git checkout master
-git pull
+cd -
