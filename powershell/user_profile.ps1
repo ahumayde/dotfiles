b962e3c1bc0368cda5b2e0e923a1ce04ea04f165
ahumayde <ahumayde@gmail.com>
2024-03-07 18:24:31 +0000

    changed powershell config

--- powershell/user_profile.ps1
+++ powershell/user_profile.ps1
@@ -1,7 +1,15 @@
-oh-my-posh init pwsh --config ~/.config/term/theme.omp.json | Invoke-Expression
+# Config Term
+C:/Users/ahuma/AppData/Local/Programs/oh-my-posh/bin/oh-my-posh.exe init pwsh --config ~/.config/term/theme.omp.json | Invoke-Expression
+
+# Env Variables
+$env:NVIM_INIT_FILE = "$env:USERPROFILE\.config\nvim\init.lua"
+$env:XDG_CONFIG_HOME = "$env:USERPROFILE\.config\"
+
+function Invoke-OhMyPoshInit { & "C:/Users/ahuma/AppData/Local/Programs/oh-my-posh/bin/oh-my-posh.exe" init pwsh --config ~/.config/term/theme.omp.json | Invoke-Expression }
 
 # Alias
 Set-Alias vim nvim
+Set-Alias omp Invoke-OhMyPoshInit
 Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'
 Set-Alias la ls | Format-Wide -Column 4 
 Set-Alias ll ls | Format-Wide -Column 4
