8d389eb215e370f623b3d4252a4174fccaa080ce
ahumayde <ahumayde@gmail.com>
2024-03-07 18:22:29 +0000

    added pa and pd for livegrep

--- nvim/plugin/telescope.lua
+++ nvim/plugin/telescope.lua
@@ -28,6 +28,8 @@ end
 vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
 vim.keymap.set("n", "<leader>pi", builtin.git_files, {})
 vim.keymap.set("n", "<leader>pl", builtin.live_grep, {})
+vim.keymap.set("n", "<leader>pa", builtin.live_grep, {})
+vim.keymap.set("n", "<leader>pd", builtin.live_grep, {})
 vim.keymap.set("n", "<leader>ps", function() fuzzyFindAfterMark() end, {})
 vim.keymap.set("n", "<leader>pg", function() builtin.grep_string({ search = vim.fn.input("Grep > ") }); end)
 vim.keymap.set("n", "<leader>ph", ":lua require('telescope.builtin').find_files({hidden=true})<CR>")
